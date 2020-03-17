2583d5eda50c748d11d88bda23c33c1b010e31a6
wjc1022 <39250043+wjc1022@users.noreply.github.com>
2020-03-17 21:17:41 +0900

    Update 20200307.py

--- 20200307.py
+++ 20200307.py
@@ -23,11 +23,15 @@
 #         print(i)
 
 # 预习题 将FizzBuzz的代码包装成一个函数，输入一个数字，报数到指定数字的函数
-def FizzBuzz(i)
-    if i % 15 == 0:
-        print('FizzBuzz')
-    elif i % 3 ==0:
-        print('Fizz')
-    elif i % 5 ==0: 
-        print('Buzz')
-print('please input 'FizzBuzz(i))
+def FizzBuzz(n):
+    for i in range(1,n+1):
+        if i%15 == 0:
+            print('FizzBuzz')
+        elif i%3 == 0:
+            print('Fizz')
+        elif i%5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+
+FizzBuzz
