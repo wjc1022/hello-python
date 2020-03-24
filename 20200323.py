9f2b680318d9a051ac10427ca889645f3afd54ec
wzy0604 <61908340+wzy0604@users.noreply.github.com>
2020-03-24 22:41:58 +0800

    Update 20200323.py

--- 20200323.py
+++ 20200323.py
@@ -1,24 +1,46 @@
 #输入一个数，判断是否是质数
-num = int(input('请输入一个数字'))
-if num < 1:
-    print('不是质数')
-if num == 1:
-    print('是质数') 
-if num == 2:
-    print('是质数') 
-for K in range(2,num):
-    if num % k==0:
-        print('不是质数')
-    else:
-        print('是质数')
+# num = int(input('请输入一个数字'))
+# if num < 1:
+#     print('不是质数')
+# if num == 1:
+#     print('是质数') 
+# if num == 2:
+#     print('是质数') 
+# j=True
+# for m in range(2,num):
+#     if num % m == 0:
+#         j=False
+#         break
+# if j :
+#     print('是质数')
+# else:
+#     print('no是质数')
+
+# num = int(input('请输入一个数字'))
+# if num < 1:
+#     print('不是质数')
+# if num == 1:
+#     print('是质数') 
+# if num == 2:
+#     print('是质数') 
+# for m in range(2,num):
+#     if num % m == 0:
+#         print('no是质数')
+#         break
+# else:
+#     print('是质数')
+
 
 # 1.题目：有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
 # 程序分析：可填在百位、十位、个位的数字都是1、2、3、4。组成所有的排列后再去 掉不满足条件的排列。
-for a in range (1,5):
-    for b in range (1,5):
-        for c in range (1,5):
-            if a!=b and b!=c and a!=c:
-                print(a,b,c)
+# for a in range (1,5):
+#     for b in range (1,5):
+#         if b==a :
+#             continue
+#         for c in range (1,5):
+#             if c==a or c==b:
+#                 continue
+#             print(a,b,c)
 
 # 2.题目：企业发放的奖金根据利润提成。
 # 利润(I)低于或等于10万元时，奖金可提10%；
@@ -29,20 +51,24 @@ for a in range (1,5):
 # 高于100万元时，超过100万元的部分按1%提成，从键盘输入当月利润I，求应发放奖金总数？
 # 程序分析：请利用数轴来分界，定位。注意定义时需把奖金定义成长整型。
 
-# i=int(input('请输入当月利润（万元）'))
-# if i <= 10 bonus=0.1*i print('奖金总数为：',bouns)
+i=int(input('请输入当月利润（万元）')
+if i < 10:
+    bonus=0.1*i 
+    print('奖金总数为：',bouns)
+if i>10 and i<20 :
+    bonus=1+(i-10)*0.075
 
 
-#!/usr/bin/python
+# !/usr/bin/python
 # -*- coding: UTF-8 -*-
  
-i = int(raw_input('净利润:'))
-arr = [1000000,600000,400000,200000,100000,0]
-rat = [0.01,0.015,0.03,0.05,0.075,0.1]
-r = 0
-for idx in range(0,6):
-    if i>arr[idx]:
-        r+=(i-arr[idx])*rat[idx]
-        print (i-arr[idx])*rat[idx]
-        i=arr[idx]
-print r
+# i = int(raw_input('净利润:'))
+# arr = [1000000,600000,400000,200000,100000,0]
+# rat = [0.01,0.015,0.03,0.05,0.075,0.1]
+# r = 0
+# for idx in range(0,6):
+#     if i>arr[idx]:
+#         r+=(i-arr[idx])*rat[idx]
+#         print (i-arr[idx])*rat[idx]
+#         i=arr[idx]
+# print r
